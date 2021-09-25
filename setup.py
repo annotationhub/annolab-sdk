ed95b9c338a40d38320191882ba67d7087040364
lsimkins <luke@bitparadigm.com>
2021-09-25 09:08:04 -0500

    v0.2.3v0.2.3v0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'annolab',
-  version='0.2.2',
+  version='0.2.3',
   packages = find_packages(),
   author='AnnoLab',
   maintainer='Luke Simkins; Grant DeLozier',
